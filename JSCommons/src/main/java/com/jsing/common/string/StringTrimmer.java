2eea10d6009108b1a163a3afd6036a2db072bb1a
patrick.schweizer <patrick@schweizer-ing.com>
2011-10-15 20:04:20 +0700

    Made class more efficient: Less object generations.

--- JSCommons/src/main/java/com/jsing/common/string/StringTrimmer.java
+++ JSCommons/src/main/java/com/jsing/common/string/StringTrimmer.java
@@ -2,32 +2,49 @@ package com.jsing.common.string;
 
 
 public class StringTrimmer {
-    String string;
+    private String string;
+    private int start=0;
+    private int end;
 
     public StringTrimmer(String string) {
         this.string = string;
+        if(this.string==null)
+        	return;
+        this.end = this.string.length();
     }
 
     public StringTrimmer getAfterNext(String stringToSearch) {
-        int index = string.indexOf(stringToSearch);
+        if(this.string==null)
+        	return this;
+        int index = getIndex(stringToSearch);
         if (index > -1) {
-            return new StringTrimmer(string.substring(index + stringToSearch.length()));
+            this.start = Math.min(this.end, index + stringToSearch.length());
         } else {
-            return new StringTrimmer("");
+            this.start = this.end;
         }
+        return this;
     }
 
-    public StringTrimmer getBeforeNext(String stringToSearch) {
-        int index = string.indexOf(stringToSearch);
+    private int getIndex(String stringToSearch) {
+    	if(stringToSearch == null)
+    		return -1;
+		return this.string.indexOf(stringToSearch, this.start);
+	}
+
+	public StringTrimmer getBeforeNext(String stringToSearch) {
+        if(this.string==null)
+        	return this;
+        int index = getIndex(stringToSearch);
         if (index > -1) {
-            return new StringTrimmer(string.substring(0, index));
-        } else {
-            return new StringTrimmer("");
+            this.end = Math.max(this.start, index );
         }
+        return this;
     }
 
     @Override
     public String toString() {
-        return string;
+    	 if(this.string==null)
+         	return null;
+    	 return this.string.substring(this.start, this.end);
     }
 }
