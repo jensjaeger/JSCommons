07a3333f68b6bef6cb6cf3e6260c1634d1a34ea3
patrick.schweizer <patrick@schweizer-ing.com>
2011-10-15 20:02:43 +0700

    More tests

--- JSCommons/src/test/java/com/jsing/common/string/StringTrimmerTest.java
+++ JSCommons/src/test/java/com/jsing/common/string/StringTrimmerTest.java
@@ -9,12 +9,21 @@ public class StringTrimmerTest {
     @Test
     public void testGetAfterNext() {
         assertEquals("ccc", new StringTrimmer("aaaBBBccc").getAfterNext("BBB").toString());
+        assertEquals("", new StringTrimmer("aaaBBBccc").getAfterNext("xxx").toString());
+        assertEquals("", new StringTrimmer("aaaBBBccc").getAfterNext("ccc").toString());
+        assertEquals("", new StringTrimmer("aaaBBBccc").getAfterNext(null).toString());
+        assertEquals(null, new StringTrimmer(null).getAfterNext("xxx").toString());
+        assertEquals("ccc", new StringTrimmer("aaaBBBccc").getAfterNext("aaa").getAfterNext("BBB").toString());
     }
 
     @Test
     public void testGetBeforeNext() {
         assertEquals("aaa", new StringTrimmer("aaaBBBccc").getBeforeNext("BBB").toString());
-        assertEquals("aaaBBB", new StringTrimmer("aaaBBBccc").getBeforeNext("ccc").toString());
+        assertEquals("aaaBBBccc", new StringTrimmer("aaaBBBccc").getBeforeNext("xxx").toString());
+        assertEquals("", new StringTrimmer("aaaBBBccc").getBeforeNext("aaa").toString());
+        assertEquals("aaaBBBccc", new StringTrimmer("aaaBBBccc").getBeforeNext(null).toString());
+        assertEquals(null, new StringTrimmer(null).getBeforeNext("xxx").toString());
+        assertEquals("aaa", new StringTrimmer("aaaBBBccc").getBeforeNext("ccc").getBeforeNext("BBB").toString());
     }
 
 }
