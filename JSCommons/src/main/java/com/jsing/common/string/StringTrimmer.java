b3c66abd12c0b5cf5469f844a02cb82b6114e94a
patrick.schweizer <patrick@schweizer-ing.com>
2011-11-07 21:24:27 +0700

    fixed bug in getBeforeLast

--- JSCommons/src/main/java/com/jsing/common/string/StringTrimmer.java
+++ JSCommons/src/main/java/com/jsing/common/string/StringTrimmer.java
@@ -1,21 +1,20 @@
 package com.jsing.common.string;
 
-
 public class StringTrimmer {
     private String string;
-    private int start=0;
+    private int start = 0;
     private int end;
 
     public StringTrimmer(String string) {
         this.string = string;
-        if(this.string==null)
-        	return;
+        if (this.string == null)
+            return;
         this.end = this.string.length();
     }
 
     public StringTrimmer getAfterNext(String stringToSearch) {
-        if(this.string==null)
-        	return this;
+        if (this.string == null)
+            return this;
         int index = getIndex(stringToSearch);
         if (index > -1) {
             this.start = Math.min(this.end, index + stringToSearch.length());
@@ -25,26 +24,44 @@ public class StringTrimmer {
         return this;
     }
 
-    private int getIndex(String stringToSearch) {
-    	if(stringToSearch == null)
-    		return -1;
-		return this.string.indexOf(stringToSearch, this.start);
-	}
-
-	public StringTrimmer getBeforeNext(String stringToSearch) {
-        if(this.string==null)
-        	return this;
+    public StringTrimmer getBeforeNext(String stringToSearch) {
+        if (this.string == null)
+            return this;
         int index = getIndex(stringToSearch);
-        if (index > -1) {
-            this.end = Math.max(this.start, index );
+        if (index > -1 && index < this.end) {
+            this.end = Math.max(this.start, index);
         }
         return this;
     }
 
+    public StringTrimmer getBeforeLast(String stringToSearch) {
+        if (this.string == null)
+            return this;
+        int index = getLastIndex(stringToSearch);
+        if (index > -1) {
+            this.end = Math.max(this.start, index);
+        }
+        return this;
+    }
+
+    private int getIndex(String stringToSearch) {
+        if (stringToSearch == null)
+            return -1;
+        return this.string.indexOf(stringToSearch, this.start);
+    }
+
+    private int getLastIndex(String stringToSearch) {
+        if (stringToSearch == null || this.end - this.start == 0)
+            return -1;
+
+        // TODO: better implementation
+        return this.toString().lastIndexOf(stringToSearch)+this.start;
+    }
+
     @Override
     public String toString() {
-    	 if(this.string==null)
-         	return null;
-    	 return this.string.substring(this.start, this.end);
+        if (this.string == null)
+            return null;
+        return this.string.substring(this.start, this.end);
     }
 }
--- JSCommons/src/test/java/com/jsing/common/string/StringTrimmerTest.java
+++ JSCommons/src/test/java/com/jsing/common/string/StringTrimmerTest.java
@@ -18,12 +18,24 @@ public class StringTrimmerTest {
 
     @Test
     public void testGetBeforeNext() {
-        assertEquals("aaa", new StringTrimmer("aaaBBBccc").getBeforeNext("BBB").toString());
+        assertEquals("aaa", new StringTrimmer("aaaBBBcccaaaccc").getBeforeNext("BBB").toString());
         assertEquals("aaaBBBccc", new StringTrimmer("aaaBBBccc").getBeforeNext("xxx").toString());
-        assertEquals("", new StringTrimmer("aaaBBBccc").getBeforeNext("aaa").toString());
+        assertEquals("", new StringTrimmer("aaaBBBcccaaa").getBeforeNext("aaa").toString());
         assertEquals("aaaBBBccc", new StringTrimmer("aaaBBBccc").getBeforeNext(null).toString());
         assertEquals(null, new StringTrimmer(null).getBeforeNext("xxx").toString());
-        assertEquals("aaa", new StringTrimmer("aaaBBBccc").getBeforeNext("ccc").getBeforeNext("BBB").toString());
+        assertEquals("aaa", new StringTrimmer("aaaBBBcccaaa").getBeforeNext("ccc").getBeforeNext("BBB").toString());
+        assertEquals("aaa", new StringTrimmer("aaa).', ;").getBeforeNext(")").getBeforeNext(".").toString());
+    }
+    
+    @Test
+    public void testGetBeforeLast() {
+        assertEquals("aaaBBBccc", new StringTrimmer("aaaBBBcccBBBccc").getBeforeLast("BBB").toString());
+        assertEquals("aaaBBBccc", new StringTrimmer("aaaBBBccc").getBeforeLast("xxx").toString());
+        assertEquals("", new StringTrimmer("aaaBBBccc").getBeforeLast("aaa").toString());
+        assertEquals("aaaBBBccc", new StringTrimmer("aaaBBBccc").getBeforeLast(null).toString());
+        assertEquals(null, new StringTrimmer(null).getBeforeLast("xxx").toString());
+        assertEquals("aaa", new StringTrimmer("aaaBBBccc").getBeforeLast("ccc").getBeforeLast("BBB").toString());
+        assertEquals("aBBBc", new StringTrimmer("aaaBBBccc").getAfterNext("aa").getBeforeLast("cc").toString());
     }
 
 }
