d446a677c161e95eb60eacbf782dd7dc05865fc6
patrick.schweizer <patrick@schweizer-ing.com>
2011-12-24 22:37:54 +0700

    rekursive iteration hinzugefuegt

--- JSCommons/src/main/java/com/jsing/common/file/MultipleFileHandler.java
+++ JSCommons/src/main/java/com/jsing/common/file/MultipleFileHandler.java
@@ -4,11 +4,27 @@ import java.io.File;
 import java.io.IOException;
 
 public abstract class MultipleFileHandler {
+    
+    boolean isRecursive = true;
+    
 	public void iterateDir(File dir) throws IOException {
 		for (File f : dir.listFiles()) {
 			handleFile(f);
 		}
 	}
+	
+    public MultipleFileHandler setRecursive(boolean isRecursive) {
+        this.isRecursive = isRecursive;
+        return this;
+    }
 
-	protected abstract void handleFile(File f) throws IOException;
+	protected void handleDir(File f) throws IOException {
+	    if(isRecursive){
+	        iterateDir(f);
+	    }
+	}
+	
+	protected void handleFile(File f) throws IOException{
+	    // default: do nothing, can be overriden by child class
+	}
 }
